15f10b37a6082d679380e31c65f6f85a2313ec08
Paulista <logonpta@lab.pta.local>
2020-03-05 22:08:41 -0300

    implementacao da pilha

---
+++ Semestre 02/Fundamentos e Estrutura de dados/Projeto/EstruturaDeDados/src/estatica/Pilha.java
@@ -0,0 +1,64 @@
+package estatica;
+
+public class Pilha {
+    
+    private Object memo[];
+    private int MAX;
+    private int posicaoTopo;
+    
+    public Pilha(){
+        MAX = 30;
+        memo = new Object[MAX];
+        posicaoTopo = -1;
+    }
+    
+    public boolean isEmpty(){
+        return (posicaoTopo == -1);
+    }
+    
+    public boolean isFull(){
+        return (posicaoTopo+1 == MAX);
+    }
+    
+    public void push(Object x){
+        if(!isFull()){
+            posicaoTopo++;
+            memo[posicaoTopo] = x;
+        }else{
+            System.out.println("Stack Overflow");
+        }
+    }
+    
+    public Object pop(){
+        if(!isEmpty()){
+            return memo[posicaoTopo--];
+            //primeiro ele usa o valor depois decrementa, se fosse --posicaoTopo ia decrementar antes de usar
+        }else{
+            return null;
+        }
+    }
+    
+    public Object top(){
+        if(!isEmpty()){
+            return memo[posicaoTopo];
+        }else{
+            return null;
+        }
+    }
+    
+    public void print(){
+        if(!isEmpty()){
+            String temp = "";
+            for(int i = 0; i <=posicaoTopo; i++){
+                if(i < posicaoTopo){
+                    temp += memo[i]+", ";
+                }else{
+                    temp += memo[i];
+                }
+            }
+            System.out.println("Pilha = [ "+temp+" ]");
+        } else{
+            System.out.println("Stack Empty");
+        }
+    }
+}
